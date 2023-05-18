0deaf660f9f0e5818e4e16ad9605a2a337e76166
Isa <iruizord@gmail.com>
2023-05-18 15:56:55 -0600

    fixing generateMarkdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,34 +7,6 @@ function renderLicenseBadge(license) {
     return "";
   }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license !== 'no license') {
-    return `
-    [${license}](https://choosealicense.com/licenses/${license})
-      `;
-    } else {
-      return ' ';
-    }
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license !== 'no license') {
-    return `
-    ## [License](#table-of-contents)
-  
-    The application is covered under the following license:
-  
-    ${renderLicenseLink(license)}
-      `;
-    } else {
-      return ' ';
-    }
-}
-
 // TODO: Create a function to generate markdown for README
   function generateMarkdown(data) {
     return `# ${data.title} 
@@ -50,7 +22,6 @@ function renderLicenseSection(license) {
   * [Languages and technologies](#languagesandtechnologies)
   * [How to use](#HowtoUseThisApplication)
   * [Contributors](#contributors)
-  * [Testing](#testing)
   * [Questions](#questions)
   ## Features
   ${data.features}
@@ -58,11 +29,8 @@ function renderLicenseSection(license) {
   ${data.require}
   ## How to use
   ${data.usage}
-  ${renderLicenseSection(data.license)}
   ## Contributors
   ${data.contributors}
-  ## Testing
-  ${data.test}
   ## Questions
   If you have a question, send it [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
   `;
